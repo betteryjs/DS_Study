1313c337924d430ed32f37e7c00d6500d763a0da
betteryjs <aanbnbnbnb@gmail.com>
2023-07-22 10:39:52 +0800

    2023年 07月 22日 星期六 10:39:52 CST

---
+++ Code/bitOperation/printaint.cpp
@@ -0,0 +1,28 @@
+//
+// Created by yjs on 23-7-22.
+//
+
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+string pringInt( unsigned int & num){
+    string  res{};
+    for (int i = 31; i >=0 ; --i) {
+        res+=(num&(1<<i))==0 ? "0": "1";
+    }
+    return res;
+}
+
+
+int main(){
+    unsigned int c=1;
+    cout << pringInt(c)<<endl;
+
+
+
+    return 0;
+}
