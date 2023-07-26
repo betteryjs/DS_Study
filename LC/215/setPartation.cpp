b23037876dc566d75373e927e8aaabdf36d8600e
betteryjs <aanbnbnbnb@gmail.com>
2023-07-26 11:40:23 +0800

    2023年 07月 26日 星期三 11:40:23 CST

--- LC/215/setPartation.cpp
+++ LC/215/setPartation.cpp
@@ -7,58 +7,41 @@
 using namespace std;
 
 
-
 class Solution {
 public:
+    int findKthLargest(vector<int> &nums, int k) {
+        return quickSelect(nums, 0, nums.size() - 1, nums.size() - k);
+    }
 
-    int Partition(vector<int> &arr, int low, int high) {
-        int pivot = arr[low];
-        while (low < high) {
+    int quickSelect(vector<int> &arr, int lo, int hi, int target) {
+        int mid = partation(arr, lo, hi);
+        if (mid == target) return arr[mid];
+        return mid < target ? quickSelect(arr, mid + 1, hi, target) : quickSelect(arr, lo, mid - 1, target);
+    }
 
-            while (low < high && arr[high] >= pivot) high--;
-            arr[low] = arr[high];
-            while (low < high && arr[low] <= pivot) low++;
-            arr[high] = arr[low];
-
+    int partation(vector<int> &arr, int lo, int hi) {
+        swap(arr[lo], arr[lo + rand() % (hi - lo + 1)]);
+        int pivot = arr[lo];
+        while (lo < hi) {
+            while (lo < hi && arr[hi] >= pivot) hi--;
+            arr[lo] = arr[hi];
+            while (lo < hi && arr[lo] <= pivot) lo++;
+            arr[hi] = arr[lo];
 
         }
-        arr[low] = pivot;
-        return low;
-
-    }
-
-
-    int randomPartition(vector<int> &arr, int low, int high) {
-        swap(arr[low], arr[low + rand() % (high - low + 1)]);
-        return Partition(arr, low, high);
-    }
-
-    int quickSelect(vector<int> &arr, int low, int hig, int target) {
-        int p = randomPartition(arr, low, hig);
-        if (p == target) {
-            return target;
-        } else {
-            return p < target ? quickSelect(arr, p + 1, hig, target) :
-                   quickSelect(arr, low, p - 1, target);
-
-        }
-    }
-
-
-    int findKthLargest(vector<int> &nums) {
-        return quickSelect(nums, 0, nums.size() - 1, nums.size() / 2);
+        arr[lo] = pivot;
+        return lo;
     }
 };
 
 
-class Solution2{
+class Solution2 {
 public:
     int findKthLargest(vector<int> &arr, int low, int high, int k) {
-        bool flag= true;
-        int low0=0,high0=arr.size()-1;
-        while (flag){
-//            swap(arr[low], arr[low + rand() % (high - low + 1)]);
-            cout << arr[low]<<endl;
+        bool flag = true;
+        int low0 = 0, high0 = arr.size() - 1;
+        while (flag) {
+            swap(arr[low], arr[low + rand() % (high - low + 1)]);
             int pivot = arr[low];
             while (low < high) {
                 while (low < high && arr[high] >= pivot) high--;
@@ -68,22 +51,23 @@ public:
             }
             arr[low] = pivot;
 
-            if(low==k){
-                flag= false;
+            if (low == k) {
+                flag = false;
                 return low;
-            }else if(low<k){
-                low0=++low;
-                high=high0;
+            } else if (low < k) {
+                low0 = ++low;
+                high = high0;
 
-            }else {
+            } else {
 
-                high0=--high;
-                low=low0;
+                high0 = --high;
+                low = low0;
             }
         }
-
-
+        return 0;
     }
+
+
 };
 
 
@@ -92,7 +76,7 @@ int main() {
 
 
     Solution2 go;
-    auto res = go.findKthLargest(nums, 0, nums.size()-1,nums.size()/2);
+    auto res = go.findKthLargest(nums, 0, nums.size() - 1, nums.size() / 2);
     cout << "res  " << res << endl;
     cout << "nums.size()/2 " << nums.size() / 2 << endl;
     for (int i = 0; i < nums.size(); ++i) {
